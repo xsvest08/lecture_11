a2e0d09ffdc08329ec41eaa3ee9dc5df01cbf04d
Tereza Svestkova <emikbvb@seznam.cz>
2021-04-20 16:51:45 +0200

    read data

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,10 +14,18 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    if field not in {"unordered_numbers","ordered_numbers", "dna_sequence"}:
+        return None
+
+    with open (file_name, "r") as json_file:
+        dict = json.load(json_file)
+
+    return dict[field]
 
 def main():
-    pass
 
+    sequential_data = read_data("sequential.json", "dna_sequence")
+    print(sequential_data)
 
 if __name__ == '__main__':
     main()
